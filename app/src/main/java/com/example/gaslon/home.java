b7d987fad68fd4d4f6813033c7450ac1123f7813
tito <syarilt@gmail.com>
2019-12-27 11:24:12 +0700

    nambah action

--- app/src/main/java/com/example/gaslon/home.java
+++ app/src/main/java/com/example/gaslon/home.java
@@ -2,13 +2,26 @@ package com.example.gaslon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class home extends AppCompatActivity {
+    ImageView btOrderhome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        btOrderhome = findViewById(R.id.orderhome);
+        btOrderhome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(home.this, Order.class);
+                startActivity(i);
+            }
+        });
     }
 }
