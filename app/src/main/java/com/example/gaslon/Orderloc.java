cd09b7c82b1ddaee99f7193236c5eb37922db55d
Farida28 <idhafafarida98@gmail.com>
2020-01-06 13:16:02 +0700

    maps coba tambah

--- app/src/main/java/com/example/gaslon/Orderloc.java
+++ app/src/main/java/com/example/gaslon/Orderloc.java
@@ -1,16 +1,44 @@
 package com.example.gaslon;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.libraries.places.api.model.AutocompletePrediction;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.mancj.materialsearchbar.MaterialSearchBar;
+
+import java.util.List;
+
 
 public class Orderloc extends AppCompatActivity {
+    private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private PlacesClient placesClient;
+    private List<AutocompletePrediction> predictionList;
+
+    private Location mLastKnownLocation;
+    private LocationCallback locationCallback;
+
+    private MaterialSearchBar materialSearchBar;
+    private View mapView;
+    private Button btnFind;
+
+    private final float DEFAULT_ZOOM = 18;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orderloc);
+
+        materialSearchBar = findViewById(R.id.searchBar);
 
     }
 
